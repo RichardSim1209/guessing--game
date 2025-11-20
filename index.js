e8d1c1bd5449ee701ea9f47eafea52b61ccba716
Simeon Richard <ime94099@gmail.com>
2025-11-20 07:04:46 -0800

    guessing game

---
+++ index.js
@@ -0,0 +1,39 @@
+const guessInput = document.querySelector("#guessInput")
+const displayMessage = document.querySelector("#displayMsg")
+
+
+console.log(RandomNumbers)
+
+function checkGuess() {
+    const UserInput= guessInput.value;
+    
+    const RandomNumbers = Math.round(Math.random()*100)
+    
+    if(UserInput <= 100){
+      if (RandomNumbers == UserInput) {
+        displayMessage.innerHTML = `You Chose  ${UserInput} and bot Chose ${RandomNumbers} <br>🎉 You Won $2,000,000 !!`
+    } 
+
+    else if(RandomNumbers != UserInput){
+                displayMessage.innerHTML = `You Chose  ${UserInput} and bot Chose ${RandomNumbers} <br> 😔 Opps! You lose, Try again.`
+    }
+}
+
+else{
+    displayMessage.innerHTML = `Please Input A Valid Number from 1-100`;
+}
+
+guessInput.value= ""
+
+}
+
+function resetGame() {
+    RandomNumbers = Math.round(Math.random() * 100)
+    document.querySelector("#message").textContent =""
+    document.querySelector("#message").style.color =""
+    
+    document.querySelector("#guessInput").focus();
+    document.querySelector("#guessInput").value = ""
+
+    displayMessage = "reset"
+}
